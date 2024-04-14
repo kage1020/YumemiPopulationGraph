7b6f9f25827959bba1ee5a18f09f197771775c70
kage1020 <4620087@ed.tus.ac.jp>
2024-04-14 10:49:50 +0900

    Reactアプリの作成

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # YumemiPopulationGraph
+
 都道府県別の総人口推移グラフを表示するSPA（ゆめみコーディング試験）
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <main className="min-h-screen grid place-items-center">Hello World</main>
+  )
+}
+
+export default App
