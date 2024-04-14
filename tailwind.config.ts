937090adf798ff3f2c1f6679c913c500e05b551b
kage1020 <4620087@ed.tus.ac.jp>
2024-04-15 00:07:00 +0900

    グラフ表示前のローディングコンポーネントの追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,20 @@ import type { Config } from "tailwindcss"
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        flip: "flip 2s infinite",
+      },
+      keyframes: {
+        flip: {
+          "0%": { transform: "rotateX(0deg)" },
+          "25%": { transform: "rotateZ(-180deg)" },
+          "50%": { transform: "rotateX(-180deg)" },
+          "75%": { transform: "rotateZ(0deg)" },
+          "100%": { transform: "rotateX(0deg)" },
+        },
+      },
+    },
   },
   plugins: [],
 } satisfies Config
